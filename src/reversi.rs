825a835de47e80533d0863d4d6bd1edcd8fe357b
wass80 <wasss80@gmail.com>
2020-08-27 00:46:25 +0900

    Flush

--- src/reversi.rs
+++ src/reversi.rs
@@ -283,12 +283,14 @@ impl RandomPlayer {
 }
 pub fn start() -> Result<(), String> {
     use std::io::{self, BufRead};
+    use std::io::{stdout, Write};
     let mut player = RandomPlayer::new();
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let resp = player.play(&line.unwrap());
         if let Some(resp) = resp {
-            println!("{}", resp)
+            println!("{}", resp);
+            stdout().flush().unwrap();
         }
     }
     Ok(())
